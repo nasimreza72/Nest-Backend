6e080673191cc0d453e869319c99215474342916
Reza <nasimreza72>
2022-06-27 21:02:14 +0200

    added multer

--- routes/houseRouter.js
+++ routes/houseRouter.js
@@ -1,72 +1,59 @@
 import express from "express";
 import House from "../models/House.js";
-import User from "../models/User.js"
-
+import User from "../models/User.js";
 
 const houseRouter = express.Router();
 
-
 houseRouter.post("/create", async (req, res) => {
-
   try {
-    const houses = await House.create(req.body)
-    res.send(houses)
-
+    const houses = await House.create(req.body);
+    res.send(houses);
   } catch (error) {
     console.log(error);
   }
-})
+});
 
 // it will return house information
 
-houseRouter.get("/:houseId",  async(req, res) => {
-
-  const id = req.params.houseId
+houseRouter.get("/:houseId", async (req, res) => {
+  const id = req.params.houseId;
 
   try {
-    const house = await House.findById(id)
-    res.send(house)
-
+    const house = await House.findById(id);
+    res.send(house);
   } catch (error) {
     console.log(error);
   }
-
-})
+});
 
 // it will update the house (to use hosting)
 // houseRouter.patch("/:houseId", (req, res) => {
-  houseRouter.patch("/create", async(req, res) => {
-    const options = {new:true, runValidators:true};
-    try {
-      const house = await House.findOneAndUpdate(
-        req.body._id, 
-        req.body,
-        options
-      )
-      res.send(house)
-  
-    } catch (error) {
-      console.log(error);
-    }
-})
+houseRouter.patch("/create", async (req, res) => {
+  const options = { new: true, runValidators: true };
+  try {
+    const house = await House.findOneAndUpdate(req.body._id, req.body, options);
+    res.send(house);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // it will return the houses
-houseRouter.get("/getCity/:city", async(req, res) => {
-
+houseRouter.get("/getCity/:city", async (req, res) => {
   try {
-    const houseListByCity = await House.find({"address.city": req.params.city})
+    const houseListByCity = await House.find({
+      "address.city": req.params.city,
+    });
     console.log(houseListByCity);
-    res.send(houseListByCity)
-  } catch (error){
-    console.log(error)
+    res.send(houseListByCity);
+  } catch (error) {
+    console.log(error);
   }
-
-})
+});
 
 // it will return the houses, we will send lat, long as query
 //(`api/house?lat='3324.342'&long='324234.324'`)
 // we will send the features and return the filtered houses
-houseRouter.get("/", (req, res) => {})
+houseRouter.get("/", (req, res) => {});
 
 export default houseRouter;
-
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import multer from "multer";
 import userRouter from "./routes/userRouter.js";
 import houseRouter from "./routes/houseRouter.js";
 import reviewRouter from "./routes/reviewRouter.js";
 import conversationRouter from "./routes/Conversation.js";
 import { connectDb } from "./lib/db.js";
-import createError from "http-errors"
+import createError from "http-errors";
 import http from "http";
-import {Server as socketio} from "socket.io";
-import checkToken from "./middleware/checkToken.js"
-
+import { Server as socketio } from "socket.io";
+import checkToken from "./middleware/checkToken.js";
+import path from "path";
+import File from "./models/File.js";
 
 const app = express();
 const httpServer = http.createServer(app);
-const io = new socketio(httpServer,{
-    cors:{
-            origin:"*",
-            methods:["GET","POST","OPTIONS"]
-        }
-})
+const io = new socketio(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+  },
+});
+
+const multerOptions = { dest: "uploads/" };
+const upload = multer(multerOptions);
 
 dotenv.config();
 app.use(express.json());
@@ -27,31 +32,61 @@ app.use(cors());
 connectDb();
 
 // routes
-app.use("/api/user",userRouter);
-app.use("/api/house",houseRouter);
-app.use("/api/review",reviewRouter);
-app.use("/api/conversation",conversationRouter);
+app.use("/api/user", userRouter);
+app.use("/api/house", houseRouter);
+app.use("/api/review", reviewRouter);
+app.use("/api/conversation", conversationRouter);
 
+
+///////////////  Testing multer
+//////  File upload endpoint with a "handleUpload" middleware that saves files
+const handleUpload = upload.fields([{ name: "selectedFile", maxCount: 1 }]);
+app.use("/file", handleUpload, async (req, res) => {
+
+  try {
+    const fileInfo = await File.create(req.files.selectedFile[0]);
+    res.send({ fileID: fileInfo._id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log(error);
+    return;
+  }
+
+});
+///////////// End of testing multer
+
+////////// Get image after Upload
+
+app.get("/getImage/:id", async (req, res) => {
+  const file = await File.findById(req.params.id);
+  const absolutePath = path.resolve(file.path);
+
+  res.sendFile(absolutePath);
+});
+
+
+//////////   End of Get image
 
 app.use((req,res,next) => {
-    next(createError( 404, `Resource ${req.method} ${req.url} not found` ))
+  next(createError( 404, `Resource ${req.method} ${req.url} not found` ))
 })
 
+
 //............Global Error Handler..........................
-app.use((error,req,res,next) => {
-        res.status(error.status || 500).send({
-        error: error.message || "Something went wrong"
-    })
-}) 
-httpServer.listen(process.env.PORT,()=>{
-    console.log(`listening to http://localhost:${process.env.PORT}`) 
-    io.on("connection", socket=>{
-        // socket.emit("Welcome_message", `Welcome Nasim ${socket.id}`);
-        // console.log(socket)
-        socket.on("Send_Message",(data)=>{
-            console.log('data :>> ', data);
-            console.log('socket.id :>> ', socket.id);
-            io.emit(data.conversationId,data.text);
-        })
-    })
-})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    error: error.message || "Something went wrong",
+  });
+});
+httpServer.listen(process.env.PORT, () => {
+  console.log(`listening to http://localhost:${process.env.PORT}`);
+  io.on("connection", (socket) => {
+    // socket.emit("Welcome_message", `Welcome Nasim ${socket.id}`);
+    // console.log(socket)
+    socket.on("Send_Message", (data) => {
+      console.log("data :>> ", data);
+      console.log("socket.id :>> ", socket.id);
+      io.emit(data.conversationId, data.text);
+    });
+  });
+});
