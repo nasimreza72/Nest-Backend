a91c310d72258f3525fd9802f81d67d48f529be9
Ömer Iliski <omeriliski06@gmail.com>
2022-07-07 21:12:19 +0200

    added review populate

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,18 +1,27 @@
 import express from "express";
 import Review from "../models/Review.js";
+import User from "../models/User.js";
+import House from "../models/House.js"
+import createError from 'http-errors';
 
 const reviewRouter = express.Router();
 
-reviewRouter.post("/create",async(req,res)=>{
+reviewRouter.post("/create",async(req,res,next)=>{
     try {
-        const review = await Review.create(req.body)
+        const review = await Review.create(req.body);
+        const user = await User.findById(req.body.authorId);
+        user.reviews.push(review);
+        user.save();
+        const house = await House.findById(req.body.houseId);
+        house.reviews.push(review);
+        house.save();
         res.send(review);
     } catch (error) {
         next(createError(400, error.message));
     }
 })
 
-reviewRouter.get("/:houseId",async(req,res)=>{    
+reviewRouter.get("/:houseId",async(req,res,next)=>{    
     const houseId = req.params.houseId;
     try {
         const review = await Review.find({houseId});
@@ -23,7 +32,7 @@ reviewRouter.get("/:houseId",async(req,res)=>{
     }
 })
 
-reviewRouter.delete("/:reviewId", async(req,res)=>{
+reviewRouter.delete("/:reviewId", async(req,res,next)=>{
     const reviewId = req.params.reviewId;
     try {
         const review = await Review.findById(reviewId);
